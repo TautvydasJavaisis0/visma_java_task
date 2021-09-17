893fab1a9c3305ba06db8733ba98ba4e84c5b151
Tautvydas Javaisis <tautvydas.javaisis@gmail.com>
2021-09-17 17:09:53 +0300

    extra dateutil unit tests

--- src/test/java/com/example/visma_java_task/util/DateUtilTest.java
+++ src/test/java/com/example/visma_java_task/util/DateUtilTest.java
@@ -2,6 +2,10 @@ package com.example.visma_java_task.util;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +27,22 @@ class DateUtilTest {
         assertNotEquals(0, DateUtil.getDurationInMonths(startDate,endDate));
         assertNotEquals(1, DateUtil.getDurationInMonths(startDate,endDate));
         assertEquals(-1, DateUtil.getDurationInMonths(startDate,endDate));
+    }
+
+    @Test
+    @DisplayName("Should parse from string to localdate")
+    void StringToLocalDate() {
+        String date = "2021-03-31";
+        LocalDate localDate = DateUtil.stringToLocalDate(date);
+        assertEquals(date, localDate.toString());
+    }
+
+    @Test
+    @DisplayName("Should throw exception with bad date format")
+    void StringToLocalDateException() {
+        String date = "2021.03.31";
+        Throwable exception = assertThrows(DateTimeParseException.class, () -> DateUtil.stringToLocalDate(date));
+        assertEquals("Text '2021.03.31' could not be parsed at index 4", exception.getMessage());
     }
 
 }
